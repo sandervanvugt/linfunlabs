9d03f5e419bd798ead7f78d83a205e450b23b7de
sandervanvugt <mail@sandervanvugt.nl>
2023-08-11 09:26:07 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This repository is pre release and contains some test labs. If you don't know what this is, you shouldn't be using the files in this git repo
---
+++ lab1.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+echo I will now check your work, wait a few seconds please
+sleep 5
+if grep bob /etc/passwd 
+then
+	echo congratulations, you completed this lab successfully!
+else
+	echo sorry, I cant find user bob
+	echo 'do you want me to show you the solution (y/n)'?
+	read ANSWER
+	if [ $ANSWER = y ]
+	then
+		echo okay, this is what you shoul do
+		echo useradd bob
+		echo 'would you like me to clean up now so that you can try yourself (y/n)'?
+		read CLEANUP	
+		if [ $CLEANUP = y ]
+		then
+			userdel -rf bob
+			echo I finished cleaning up, you can now try again
+		else
+			echo not cleaning up anything
+			exit 9
+		fi
+	else
+		echo not showing a solution, terminating
+		exit 8
+	fi
+fi
+
